ab31417313c6f2e8a43509a36f4e810f27429a58
lmatkows <lmatkows@student.42perpignan.fr>
2026-04-30 11:38:01 +0200

    ex03 finito

---
+++ ex03/NULL_not_found.py
@@ -0,0 +1,20 @@
+def NULL_not_found(object: any) -> int:
+	status: int = 0
+	text_to_print: str = "Type not Found"
+	type_obj = type(object)
+
+	if (object is None):
+		text_to_print = f"Nothing: {object} {type_obj}"
+	elif (type_obj == float and object != object):
+		text_to_print = f"Cheese: {object} {type_obj}"
+	elif (type_obj == int and object == 0):
+		text_to_print = f"Zero: {object} {type_obj}"
+	elif (type_obj == str and object == ""):
+		text_to_print = f"Empty: {object} {type_obj}"
+	elif (type_obj == bool and object == False):
+		text_to_print = f"Fake: {object} {type_obj}"
+	else :
+		status = 1
+
+	print (text_to_print)
+	return status
