b1a963bfe6d1c72f3ca1f237dd1a19f30b62e5f6
lmatkows <lmatkows@student.42perpignan.fr>
2026-05-20 13:42:32 +0200

    petite modif sur ex01

--- ex01/format_ft_time.py
+++ ex01/format_ft_time.py
@@ -7,4 +7,4 @@ scientific_time = f"{time_now:.3e}"
 text_bef = "Seconds since January 1, 1970: "
 text_aft = " in scientific notation"
 print(f"{text_bef}{seconds_time} or {scientific_time}{text_aft}")
-print(time.ctime(time_now))
+print(time.strftime("%a %d %Y"))
